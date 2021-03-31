682440b5d51be157ecfb01cae80831830c9f890c
Adam Urish <adamurish@gmail.com>
2021-03-31 15:21:35 -0500

    update step 4 to add new tests (placeholder tests for now)

---
+++ responses/t-test_module.py
@@ -0,0 +1,11 @@
+import pytest
+
+class TestModules():
+    def test_initpy():
+        assert True
+
+    def test_skeleton_code():
+        assert True
+
+    def test_docstrings():
+        assert True
