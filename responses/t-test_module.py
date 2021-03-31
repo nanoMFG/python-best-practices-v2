1e50151dc53f7d2feecceb17306f56da5b01615b
Adam Urish <adamurish@gmail.com>
2021-03-31 16:09:49 -0500

    begin filling in tests

--- responses/t-test_module.py
+++ responses/t-test_module.py
@@ -1,11 +1,17 @@
+import os
 import pytest
 
 class TestModules():
-    def test_initpy():
-        assert True
+    def test_initpy(self):
+        assert os.path.isfile('src/digit_reader/__init__.py')
+        assert os.path.isfile('src/digit_reader/model/__init__.py')
 
-    def test_skeleton_code():
-        assert True
+    def test_skeleton_code(self):
+        assert os.path.isfile('src/digit_reader/model/model.py')
+        import digit_reader.model.MNISTModel
+        model = MNISTModel()
+        model.train_model()
+        model.evaluate_model()
 
-    def test_docstrings():
+    def test_docstrings(self):
         assert True
