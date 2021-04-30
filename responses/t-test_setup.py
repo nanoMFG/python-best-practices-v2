96b4f9e31c7ae592259e95e192e4bac451c3eacb
﻿Adam Urish <adamurish@gmail.com>
2021-04-30 16:19:40 -0500

    Create t-test_setup.py

---
+++ responses/t-test_setup.py
@@ -0,0 +1,43 @@
+import os
+import pytest
+
+
+class TestSetup():
+    def test_fields(self):
+#       python_requires='>=3.0',
+#     install_requires=[
+#         'django',
+#         'pandas'
+#     ]
+#     name="mnist_example-nanoMFG", # Replace with your own username
+#     version="0.0.1",
+#     author="Example Author",
+#     author_email="author@example.com",
+#     description="A small example package",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/pypa/sampleproject",
+#     packages=setuptools.find_packages()
+        with open('setup.py') as setup_file:
+            all_satisfied = True
+            all_fields = {
+              'python_requires' : False,
+              'install_requires' : False,
+              'name' : False,
+              'version' : False,
+              'author' : False,
+              'author_email' : False,
+              'description' : False,
+              'long_description' : False,
+              'long_description_content_type' : False,
+              'url' : False,
+              'packages' : False}
+            for line in setup_file:
+                for key in all_fields:
+                    if key in line:
+                        all_fields[key] = True
+            for key in all_fields:
+                if not all_fields[key]
+                    all_satisfied = False
+        assert all_satisfied
+            
