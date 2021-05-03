bed4f9bd6a4bcf81d5fdae6ab7e8bcc93ee40d12
briancy02 <64820854+briancy02@users.noreply.github.com>
2021-05-04 03:32:20 +0900

    Update t-test_mnist.py

--- responses/t-test_mnist.py
+++ responses/t-test_mnist.py
@@ -2,4 +2,7 @@ from digit_reader.model.helpers import prepare_data
 from digit_reader.model.model import MNISTModel
 
 def test_classify_image():
-    assert False
+    (x_train, y_train), (x_test, y_test) = prepare_data()
+    model = MNISTModel()
+    model.train_model(x_train, y_train, epochs=2)
+    assert model.classify_image(x_test[0]) == 7
