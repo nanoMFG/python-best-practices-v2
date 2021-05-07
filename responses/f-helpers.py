50fb23405bf89e1871893a361c236552e4bae72b
briancy02 <64820854+briancy02@users.noreply.github.com>
2021-05-07 22:19:07 +0900

    Create f-helpers.py

---
+++ responses/f-helpers.py
@@ -0,0 +1,34 @@
+from tensorflow import keras
+import numpy as np
+
+
+num_classes = 10
+"""Defines how many possible numbers to classify"""
+
+input_shape = (28, 28, 1)
+"""Defines the shape of the input images, 28x28 with one color channel"""
+
+
+def prepare_data():
+    """Load dataset and preprocess data
+    Returns:
+        A tuple of pre-processed training data
+    """
+
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+    print("x_train shape:", x_train.shape)
+    print(x_train.shape[0], "train samples")
+    print(x_test.shape[0], "test samples")
+
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+    return (x_train, y_train), (x_test, y_test)
